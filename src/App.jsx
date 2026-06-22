ef55310b16fef4646574fba96de1a03a6d03dcd4
fctornadooficial-rgb <fctornado.oficial@gmail.com>
2026-06-22 06:13:30 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import AdminPanel from './AdminPanel';
 import { AuthProvider, useAuth } from './AuthContext';
